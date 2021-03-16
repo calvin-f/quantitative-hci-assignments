8fc76af8c878866f7cb9bedec95a597f75651b37
SophiaBieri <64488355+SophiaBieri@users.noreply.github.com>
2021-03-16 11:59:48 +0100

    Add files via upload

---
+++ sophia/C1.08 wide_tall.R
@@ -0,0 +1,45 @@
+# Wide to tall
+# ===
+# Goal: Change the meal preference to the tidy data format:
+#   
+#   * Each variable forms a column. (`name`, `meal`, `food`)
+#   * Each observation forms a row.
+#   * Each type of observational unit forms a table.
+#   
+# Functions: pivot_longer()
+# Example output: 
+#
+#      # A tibble: 9 x 3
+#        name    meal      food       
+#        <chr>   <chr>     <chr>      
+#      1 Tomo    breakfast Yogurt     
+#      2 Susanne breakfast Boiled eggs
+#      3 Rajit   breakfast Soup       
+#      4 Tomo    lunch     Schnitzel  
+#      5 Susanne lunch     Spaghetti  
+#      6 Rajit   lunch     Burger     
+#      7 Tomo    dinner    Rissotto   
+#      8 Susanne dinner    Steak      
+#      9 Rajit   dinner    Salad 
+#
+# Relevant R4DS chapter: https://cran.r-project.org/web/packages/tidyr/vignettes/tidy-data.html#tidy-data
+
+#===============================================================================
+
+library(tidyverse)
+
+wt_input <- tribble(
+  ~name,     ~breakfast,    ~lunch,      ~dinner,
+  "Tomo",    "Yogurt",      "Schnitzel", "Rissotto",
+  "Susanne", "Boiled eggs", "Spaghetti", "Steak",
+  "Rajit",   "Soup",        "Burger",    "Salad")
+
+
+# Answer:
+the_answer <- 
+  wt_input %>%
+  pivot_longer(-name, names_to = "meal", values_to = "food") %>%
+  arrange(factor(meal, levels = c("breakfast","lunch","dinner")))
+
+# TODO: replace NULL with your answer
+
---
+++ sophia/C1.09 tall_wide.R
@@ -0,0 +1,47 @@
+# Tall to wide
+# ===
+# Goal: Sum up the number of glances at a mobile phone during weekdays and weekends
+# Functions: pivot_wider(), mutate(), select()
+# Example output: 
+#
+#      # A tibble: 2 x 3
+#        participant_id weekdays_count weekends_count
+#        <chr>                   <dbl>          <dbl>
+#      1 P01                        89             36
+#      2 P02                       135             47
+#      
+# Relevant R4DS chapter: https://cran.r-project.org/web/packages/tidyr/vignettes/tidy-data.html#tidy-data
+
+
+#===============================================================================
+
+library(tidyverse)
+
+tw_input <- tribble(
+  ~participant_id, ~day_of_week, ~glance_count,
+  "P01",           "Mo",         22,
+  "P01",           "Tu",         17,
+  "P01",           "We",         10,
+  "P01",           "Th",         27,
+  "P01",           "Fr",         13,
+  "P01",           "Sa",         26,
+  "P01",           "So",         10,
+  "P02",           "Mo",         28,
+  "P02",           "Tu",         26,
+  "P02",           "We",         27,
+  "P02",           "Th",         30,
+  "P02",           "Fr",         24,
+  "P02",           "Sa",         29,
+  "P02",           "So",         18)
+
+
+# Answer:
+the_answer <- 
+  tw_input %>%
+  pivot_wider(names_from = day_of_week,values_from = glance_count) %>%
+  mutate(weekdays_count = rowSums(.[2:6])) %>%
+  mutate(weekends_count = rowSums(.[7:8])) %>%
+  select("participant_id","weekdays_count","weekends_count")
+
+# TODO: replace NULL with your answer
+
