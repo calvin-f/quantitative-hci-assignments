c2c22496ad18c02b1053eaf59abbdf2c4f6a7b5c
SophiaBieri <64488355+SophiaBieri@users.noreply.github.com>
2021-04-22 12:02:19 +0200

    Add files via upload

---
+++ sophia/C2.06 plot_grid.R
@@ -0,0 +1,39 @@
+# Combine multiple types of plot in one file.
+# ===
+# Goal: Combine the given two plots side-by-side on one view. 
+#       Set the width of the two plots to be in 2:3 ratio with rel_widths()
+# 
+# Example output: `goals/C2.06 plot_grid_goal.png`
+# 
+# Functions: plot_grid() from the package `cowplot`
+#
+# Relevant resource: Introduction to plot_grid(): https://wilkelab.org/cowplot/articles/plot_grid.html
+
+#===============================================================================
+library(tidyverse)
+import::from(cowplot, plot_grid)
+source("R/prepare_data.R")
+
+mp_input_1 <- 
+  data_knobology_within %>% 
+  ggplot(aes(x = device, y = time)) +
+  geom_pointrange(stat = "summary", fun.data = mean_cl_normal) 
+
+mp_input_2 <- 
+  data_knobology_within %>% 
+  ggplot(aes(x = vision, y = time)) +
+  geom_pointrange(stat = "summary", fun.data = mean_cl_normal) 
+
+
+# Answer:
+plot_grid(mp_input_1,mp_input_2,rel_widths=c(2,3))
+
+#-------------------------------------------------------------------------------
+# Reflect: What is the difference between the capability  of the `plot_grid()` and the `facet_…()` functions? 
+# The reflection is not graded.
+#
+# Your reflection:
+# 
+# 
+#-------------------------------------------------------------------------------
+
---
+++ sophia/C2.07 zoom.R
@@ -0,0 +1,41 @@
+# Zoom
+# ===
+# Goal: Zoom the range of the vertical-axis to be between 2 and 4. 
+#       Ensure that the mean and error bars do not change.
+#
+# Example output: `goals/C2.07 zoom_goal.png`
+#
+# Function: coord_cartesian()
+# 
+# Relevant resources: 
+#   * R4DS zooming: https://r4ds.had.co.nz/graphics-for-communication.html?q=limit#zooming
+#   * scales: https://ggplot2-book.org/mastery.html#mastering-scales
+#   * coordinates: https://ggplot2-book.org/mastery.html#coordinate-systems
+
+#===============================================================================
+
+library(tidyverse)
+import::from(ggbeeswarm, geom_beeswarm)
+source("R/prepare_data.R") 
+
+zm_input <- 
+  data_knobology_resampled %>% 
+  ggplot(aes(x = device, y = time)) +
+  geom_beeswarm(alpha = 0.2) +
+  geom_pointrange(stat = "summary", fun.data = mean_cl_normal) 
+
+
+# Answer:
+zm_input +
+  coord_cartesian(ylim = c(2, 4))      # TODO: replace NULL with your answer
+  
+#-------------------------------------------------------------------------------
+# Try and observe: The function `scale_y_continuous()` could also be used to zoom in as well.
+#                  However, you will receive some warnings. Why do these warnings appear?
+# The observation is not graded.
+# 
+# Your observation:
+# 
+# 
+#-------------------------------------------------------------------------------
+  
