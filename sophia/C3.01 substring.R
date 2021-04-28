a2afadb3dd9e12aaa0d8afc1892030d0b49baacf
SophiaBieri <64488355+SophiaBieri@users.noreply.github.com>
2021-04-28 14:57:14 +0200

    Add files via upload

---
+++ sophia/C3.01 substring.R
@@ -0,0 +1,32 @@
+# Cutting text to pieces and putting them together
+# ===
+# Goal: Combine the `user_id` and the `team_id` to the format like this example: `T01P01`. Store the results in the `combined_id` column.
+# Functions: mutate(), str_sub(), str_c()
+# Example output: 
+#
+#      # A tibble: 3 x 3
+#        user_id team_id combined_id
+#        <chr>   <chr>   <chr>      
+#      1 P0001   01      T01P01     
+#      2 P0002   02      T02P02     
+#      3 P0003   03      T03P03  
+
+#
+# Relevant R4DS chapter: https://r4ds.had.co.nz/strings.html
+
+#===============================================================================
+
+library(tidyverse)
+
+su_input <- tribble(
+  ~user_id, ~team_id,
+  "P0001",  "01",
+  "P0002",  "02",
+  "P0003",  "03")
+
+# Answer:
+the_answer <- 
+  su_input %>%
+  mutate(combined_id =str_c("T",team_id,str_sub(user_id,1,2),str_sub(user_id,5,5)))                # TODO: replace NULL with your answer
+
+
